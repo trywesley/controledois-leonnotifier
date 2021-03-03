445f104bc03902113ed3ccad4bf1e4a40148d9b3
trywesley <66664225+trywesley@users.noreply.github.com>
2021-03-03 13:37:51 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@ require('dotenv').config()
 
 const DiscordClient = require("./structures/discord/Client")
 const Client = new DiscordClient(process.env.TOKEN)
+
+Client.on("message", message => {
+
+if(!message.content.startsWith("1")) return
+message.reply("555")
+})
