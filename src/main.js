fc19eead6eb74d3866af48a05d2e95f790ee0b03
trywesley <66664225+trywesley@users.noreply.github.com>
2021-03-03 13:44:18 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 require('dotenv').config()
 
+const RSSParser = require("rss-parser")
+const Parser = new RSSParser()
 const DiscordClient = require("./structures/discord/Client")
 const Client = new DiscordClient(process.env.TOKEN)
+
+const parsed = Parser.parseURL("https://www.youtube.com/feeds/videos.xml?channel_id=UCSRmIdDu1XFDr2iViBvcEDA")
+console.log(parsed)
 
 Client.on("message", message => {
 
 if(!message.content.startsWith("1")) return
 message.reply("555")
+
 })
