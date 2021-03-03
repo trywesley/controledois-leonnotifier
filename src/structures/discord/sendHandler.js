8e572ed4951a593a32963231e5320593f5e6efeb
trywesley <66664225+trywesley@users.noreply.github.com>
2021-03-03 16:19:15 -0400

    Update sendHandler.js

--- src/structures/discord/sendHandler.js
+++ src/structures/discord/sendHandler.js
@@ -1,6 +1,8 @@
+const Discord = require("discord.js")
+
 module.exports = async function sH (video, Client) {
     const channel = Client.channels.cache.get(process.env.DISCORD_CHANNEL)
-    const embed = new Client.Discord.MessageEmbed()
+    const embed = new Discord.MessageEmbed()
     .setTitle("teste")
 
     channel.send(embed)
