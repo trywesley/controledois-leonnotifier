52ca4c85537d4a72c2d7a1bcced0296c01156821
trywesley <66664225+trywesley@users.noreply.github.com>
2021-03-03 13:12:37 -0400

    Create Client.js

---
+++ src/structures/discord/Client.js
@@ -0,0 +1,16 @@
+const { Client } = require("discord.js")
+const loadPRTYPS = require("../../strc/prtyps/proto")
+
+module.exports = class LeonC2NotifierClient extends Client {
+	constructor(token) {
+		super()
+		super.login(token)
+                
+		this.acess = ["451920956768649226"]
+                loadPRTYPS.load()
+	}
+        
+	get rssUsage () {
+		return Math.round(process.memoryUsage().rss / 1024 / 1024)
+	}
+}
