a61bd09cfa21ab44abd6ec8cfd630110585b09f5
trywesley <66664225+trywesley@users.noreply.github.com>
2021-03-04 19:18:08 -0400

    Update Client.js

--- src/structures/discord/Client.js
+++ src/structures/discord/Client.js
@@ -5,7 +5,7 @@ module.exports = class LeonC2NotifierClient extends Client {
 		super()
 		super.login(token)
                 
-		this.acess = ["451920956768649226"]
+		this.acess = ["451920956768649226", "317714973784670208"]
                 this.lastVideoID = null
 	}
         
