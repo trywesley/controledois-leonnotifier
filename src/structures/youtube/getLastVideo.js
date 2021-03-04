0f983e4291f66a9efa01749089a83ec274415970
trywesley <66664225+trywesley@users.noreply.github.com>
2021-03-04 13:10:03 -0400

    Update getLastVideo.js

--- src/structures/youtube/getLastVideo.js
+++ src/structures/youtube/getLastVideo.js
@@ -2,7 +2,7 @@ const RSSParser = require("rss-parser")
 const Parser = new RSSParser()
 
 module.exports = async function gLV () {
-    const parsed = await Parser.parseURL("https://www.youtube.com/feeds/videos.xml?channel_id=UCV306eHqgo0LvBf3Mh36AHg")
+    const parsed = await Parser.parseURL("https://www.youtube.com/feeds/videos.xml?channel_id=UCbTVTephX30ZhQF5zwFppBg")
     const lastVideo = parsed.items.sort((a, b) => {
         const A = new Date(a.pubDate).getTime()
         const B = new Date(b.pubDate).getTime()
