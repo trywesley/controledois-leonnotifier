8d5dcad666a73cd89eebfe5a5b6f31d23f00c4b0
trywesley <66664225+trywesley@users.noreply.github.com>
2021-03-04 19:43:03 -0400

    Update Client.js

--- src/structures/discord/Client.js
+++ src/structures/discord/Client.js
@@ -6,15 +6,30 @@ module.exports = class LeonC2NotifierClient extends Client {
 		super.login(token)
                 
 		this.acess = ["451920956768649226", "317714973784670208"]
-                this.lastVideoID = null
+                this._logs = []
 	}
         
 	get status () {
             const object = {
                 ping: this.ws.ping,
                 rssUsage: Math.round(process.memoryUsage().rss / 1024 / 1024) + "MB",
-                checkInterval: Client.check_interval,
+                checkInterval: Client.check_interval, 
+                logs: this._logs.join("\n")
             }
-		return object
+            return object
 	}
+
+        get logs () {
+            return this._logs.join("\n")
+        }
+
+        log (text) {
+            console.log(text)
+            
+            if(this._logs.length && this._logs[this._logs.length] === text) return
+            if(this._logs.length > 10) {
+                this._logs.pop()
+            }
+            this._logs.push(text)
+        }
 }
