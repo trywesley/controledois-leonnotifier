68542191575d9786299a608b5ac2bd561d235470
trywesley <66664225+trywesley@users.noreply.github.com>
2021-03-04 19:11:01 -0400

    Update evaluate.js

--- src/structures/discord/commands/evaluate.js
+++ src/structures/discord/commands/evaluate.js
@@ -1,1 +1,17 @@
+const util = require('util')
 
+module.exports = async function evaluate(Client, content, channel) {
+    const regex = /<@!?(\d{16,18})>/g
+    content = content.replace(reg, 'Client.users.cache.get("$1")')
+    content = content.split(" ").slice(1).join(" ")
+ 
+    let result
+    try {
+        const evaled = await eval(content)
+        result = util.inspect(evaled, { compact: true, depth: 0 });
+    } catch (error) {
+        result = error.toString()
+    }
+        
+    channel.send(result, {code: 'js'})
+}
