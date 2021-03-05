f7977c2f664f19dacf9f4c21a0bef5e4a85752ce
trywesley <66664225+trywesley@users.noreply.github.com>
2021-03-04 20:11:02 -0400

    Update sendHandler.js

--- src/structures/discord/sendHandler.js
+++ src/structures/discord/sendHandler.js
@@ -1,10 +1,13 @@
 module.exports = async function sH (id, Client, trueorfalse) {
     const channelRCDN = Client.channels.cache.get(process.env.DISCORD_CHANNEL)    
     const channelSAVE = Client.channels.cache.get(process.env.SAVE_CHANNEL)
+    let message = "Acabei de publicar um vídeo: " + id
 
     if(trueorfalse) {
         channelRCDN.send("https://youtu.be/" + id)
+        message = message + " (Leon)"
     }
 
     channelSAVE.send("https://youtu.be/" + id)
+    Client.log(message)
 }
