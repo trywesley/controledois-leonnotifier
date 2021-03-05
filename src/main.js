d4b52118bfe39ad3809d04720b51bb894263115a
trywesley <66664225+trywesley@users.noreply.github.com>
2021-03-04 21:37:31 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,39 @@ require('dotenv').config()
 
 const { getLastVideo, getVideoDetails } = require("./structures/youtube")
 const { DiscordClient, sendHandler } = require("./structures/discord")
-const Client = new DiscordClient(process.env.TOKEN)
+const Client = new DiscordClient(process.env.TOKEN, {
+  disabledEvents: [
+    "GUILD_MEMBER_ADD",
+    "GUILD_MEMBER_REMOVE",
+    "GUILD_MEMBER_UPDATE",
+    "GUILD_MEMBERS_CHUNK",
+    "GUILD_ROLE_CREATE",
+    "GUILD_ROLE_DELETE",
+    "GUILD_ROLE_UPDATE",
+    "GUILD_BAN_ADD",
+    "GUILD_BAN_REMOVE",
+    "CHANNEL_CREATE",
+    "CHANNEL_DELETE",
+    "CHANNEL_UPDATE",
+    "CHANNEL_PINS_UPDATE",
+    "MESSAGE_CREATE",
+    "MESSAGE_DELETE",
+    "MESSAGE_UPDATE",
+    "MESSAGE_DELETE_BULK",
+    "MESSAGE_REACTION_ADD",
+    "MESSAGE_REACTION_REMOVE",
+    "MESSAGE_REACTION_REMOVE_ALL",
+    "USER_UPDATE",
+    "USER_NOTE_UPDATE",
+    "USER_SETTINGS_UPDATE",
+    "PRESENCE_UPDATE",
+    "VOICE_STATE_UPDATE",
+    "TYPING_START",
+    "VOICE_SERVER_UPDATE",
+    "RELATIONSHIP_ADD",
+    "RELATIONSHIP_REMOVE"
+  ]
+})
 
 async function check ()  {
     const lastVideo = await getLastVideo()
