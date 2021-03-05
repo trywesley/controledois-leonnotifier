d303d65d6e3fb3d1c9425b999a3507006348fba0
trywesley <66664225+trywesley@users.noreply.github.com>
2021-03-04 20:44:36 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,14 +7,12 @@ const Client = new DiscordClient(process.env.TOKEN)
 async function check ()  {
     const lastVideo = await getLastVideo()
     const lastVideoID = lastVideo.id.split(":")[2]
-    const savedLastVideosID = Client.lastVideosID
 
     Client.log("Estou aguardando novos vídeos")
     
-    if(savedLastVideosID.includes(lastVideoID)) return
+    if(Client.lastVideosID.includes(lastVideoID)) return
     const checked = await checkLeon(lastVideoID)
-    const trueorfalse = checked ? true : false
-    sendHandler(lastVideoID, Client, trueorfalse)
+    sendHandler(lastVideoID, Client, checked)
     
     Client.lastVideosID.pop()
     Client.lastVideosID.splice(0, 0, lastVideoID)
@@ -26,16 +24,3 @@ async function checkLeon (id) {
 }
 
 Client.check_interval = setInterval(check, 60000)
-
-Client.on("message", message => {
-    if(!Client.acess.includes(message.author.id)) return
-    if(message.content.startsWith("evaluate")) {
-        require("./structures/discord/commands/evaluate")(Client, message.content, message.channel)
-    }
-})
-
-Client.on("ready", async () => {
-    const msgs = await Client.channels.cache.get(process.env.SAVE_CHANNEL).messages.fetch({limit: 5}).then(msgs => msgs.map(msg => msg.content.split("/")[3]))
-    Client.lastVideosID = msgs
-    Client.log(Date.now() + " - Estou online novamente")
-})
