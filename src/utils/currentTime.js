00aedee7696cc9000c396a4876bf19dfb60c0b50
trywesley <66664225+trywesley@users.noreply.github.com>
2021-03-05 14:51:35 -0400

    Create currentTime.js

---
+++ src/utils/currentTime.js
@@ -0,0 +1,10 @@
+module.exports = function cT {
+    const d = new Date()
+
+    let hour = d.getHours() - 3
+    hour = hour < 0 ? 24 + hour : hour
+    hour = hour.toString().padStart(2, "0") 
+    const minutes = d.getMinutes().toString().padStart(2, "0")
+
+    return hour+":"+minutes
+}
